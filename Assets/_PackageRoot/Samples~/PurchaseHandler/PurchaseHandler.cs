d1f869db41f53484f2fa2e2d6d73c883176ca062
Andrey <40003779+KelpieT@users.noreply.github.com>
2023-11-16 23:02:26 +0200

    Fix sample PurchaseHandler

--- Assets/_PackageRoot/Samples~/PurchaseHandler/PurchaseHandler.cs
+++ Assets/_PackageRoot/Samples~/PurchaseHandler/PurchaseHandler.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
-namespace Viter.Localization
+namespace Viter.IAPSystem
 {
     public class PurchaseHandler : MonoBehaviour
     {
