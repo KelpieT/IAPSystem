520d5b60daa8722dfc2897dee5072e5b7e601ac3
Andrey <40003779+KelpieT@users.noreply.github.com>
2023-11-16 22:53:56 +0200

    Add samples

---
+++ Samples/PurchaseHandler/PurchaseHandler.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Viter.Localization
+{
+    public class PurchaseHandler : MonoBehaviour
+    {
+        /// <summary>
+        /// This function is called when the behaviour becomes enabled or active.
+        /// </summary>
+        private void OnEnable()
+        {
+            IAPManager.OnPurchaseSuccessfulEvent += OnPurchaseSuccessful;
+            IAPManager.OnPurchaseFailedEvent += OnPurchaseFailed;
+            IAPManager.OnRestoreCompletedEvent += OnRestoreCompleted;
+        }
+
+        /// <summary>
+        /// This function is called when the behaviour becomes disabled or inactive.
+        /// </summary>
+        private void OnDisable()
+        {
+            IAPManager.OnPurchaseSuccessfulEvent -= OnPurchaseSuccessful;
+            IAPManager.OnPurchaseFailedEvent -= OnPurchaseFailed;
+            IAPManager.OnRestoreCompletedEvent -= OnRestoreCompleted;
+        }
+
+        /// <summary>
+        /// Purchase Rewards will be processed from here. You can adjust your code based on your requirements.
+        /// </summary>
+        /// <param name="productInfo"></param>
+        private void OnPurchaseSuccessful(ProductInfo productInfo)
+        {
+            RewardType rewardType = ((RewardType)productInfo.rewardType);
+
+            switch (rewardType)
+            {
+                case RewardType.REMOVE_ADS:
+                    //Remove ads reward
+                    break;
+                case RewardType.GEMS:
+
+                    int rewardAmount = productInfo.rewardAmount;
+                    //Reward purchase
+                    break;
+
+                case RewardType.OTHER:
+                    break;
+            }
+            Product product = IAPManager.Instance.GetProductFromSku(productInfo.productName);
+        }
+
+        private void OnPurchaseFailed(string reason)
+        {
+
+        }
+
+        private void OnRestoreCompleted(bool result)
+        {
+            if (result)
+            {
+            }
+            else
+            {
+            }
+        }
+    }
+}
+
+
